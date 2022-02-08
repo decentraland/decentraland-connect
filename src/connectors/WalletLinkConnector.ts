050229a3fd710fbdead20b9b51c84115716cef04
Fernando Zavalia <24811313+fzavalia@users.noreply.github.com>
2022-02-08 12:33:25 -0300

    chore: App name

--- src/connectors/WalletLinkConnector.ts
+++ src/connectors/WalletLinkConnector.ts
@@ -1,13 +1,14 @@
 import { ChainId } from '@dcl/schemas'
-import { WalletLinkConnector as BaseInjectedConnector } from '@web3-react/walletlink-connector'
+import { WalletLinkConnector as BaseWalletLinkConnector } from '@web3-react/walletlink-connector'
 import { RPC_URLS } from './NetworkConnector'
 
-export class WalletLinkConnector extends BaseInjectedConnector {
+const APP_NAME = 'Decentraland'
+
+export class WalletLinkConnector extends BaseWalletLinkConnector {
   constructor(chainId: ChainId) {
     super({
-      supportedChainIds: [chainId],
       url: RPC_URLS[chainId],
-      appName: ''
+      appName: APP_NAME
     })
   }
 }
