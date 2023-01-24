14481dabce4fe6833b137daa304de941d157a59b
frami <i@2fd.me>
2023-01-23 22:42:31 -0300

    fix: import @dcl/schemas

--- src/connectors/NetworkConnector.ts
+++ src/connectors/NetworkConnector.ts
@@ -1,5 +1,5 @@
-import { ProviderType } from '@dcl/schemas'
-import { ChainId } from '@dcl/schemas/dist/dapps/chain-id'
+import type { ChainId } from '@dcl/schemas/dist/dapps/chain-id'
+import { ProviderType } from '@dcl/schemas/dist/dapps/provider-type'
 import { NetworkConnector as BaseNetworkConnector } from '@web3-react/network-connector'
 import { getConfiguration } from '../configuration'
 
