c54b1364c2b89056e8f3deb123effebe454e0435
Gabriel Diaz <gabrieldiaz31@gmail.com>
2024-06-17 17:45:03 -0300

    fix: Use Promise.race instead of promises inside promises

--- src/connectors/InjectedConnector.ts
+++ src/connectors/InjectedConnector.ts
@@ -13,19 +13,15 @@ export class InjectedConnector extends BaseInjectedConnector {
   }
 
   async activate(): Promise<ConnectorUpdate> {
-    return new Promise((resolve, reject) => {
-      // Set a timeout to reject the promise if the wallet is not unlocked
-      const timeout = setTimeout(() => {
+    // Set a timeout to reject the promise if the wallet is not unlocked
+    const timeoutPromise = new Promise<ConnectorUpdate>((_, reject) => {
+      setTimeout(() => {
         reject(new ErrorUnlockingWallet())
       }, UNLOCK_WALLET_TIMEOUT)
-
-      super
-        .activate()
-        .then(resolve)
-        .catch(reject)
-        .finally(() => {
-          clearTimeout(timeout)
-        })
     })
+
+    const activatePromise = super.activate()
+
+    return Promise.race([activatePromise, timeoutPromise])
   }
 }
